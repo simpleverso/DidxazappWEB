027f3634cf952f4995ad7472fa6e17087444cd82
Gonzalo santiago martinez <simpleverso@users.noreply.github.com>
2024-02-15 20:06:47 -0600

    changes

--- components/LoadingSpinner.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const LoadingSpinner = () => {
-  return <div className="loading-spinner" aria-label="Loading..."></div>;
-}
-
-export default LoadingSpinner;
--- utils/getLocalStorage.js
+++
@@ -1,3 +0,0 @@
-export default function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
-}
